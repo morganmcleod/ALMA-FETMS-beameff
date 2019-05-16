f526fede5f176564d370fc152f46381501517067
Morgan McLeod <mmcleod@nrao.edu>
2019-05-16 11:31:53 -0400

    Ported from https://github.com/scikit-image/scikit-image/blob/master/skimage/restoration/unwrap_2d_ljmu.c having license (Modified BSD) from https://github.com/scikit-image/scikit-image Instead of earlier version

--- beameff2_CPP/unwrap2D/unwrap2D.h
+++ beameff2_CPP/unwrap2D/unwrap2D.h
@@ -1,10 +1,11 @@
 #ifndef UNWRAP2D_H_
 #define UNWRAP2D_H_
-// Cloned from https://github.com/geggo/phase-unwrap
-// See unwrap2D.cpp for non-commercial license terms
+// cloned from https://github.com/scikit-image/scikit-image/blob/master/skimage/restoration/unwrap_2d_ljmu.c
+// having license (Modified BSD) from https://github.com/scikit-image/scikit-image
 
-void unwrap2D(float* wrapped_image, float* unwrapped_image, unsigned char* input_mask,
-              int image_width, int image_height,
-              int wrap_around_x, int wrap_around_y);
+void unwrap2D(double *wrapped_image, double *UnwrappedImage,
+              unsigned char *input_mask, int image_width, int image_height,
+              int wrap_around_x, int wrap_around_y,
+              char use_seed = 0, unsigned int seed = 0);
 
 #endif /* UNWRAP2D_H_ */
